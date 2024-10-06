a7d0f6304c43376268476ddb2397a4fc302459d3
Mayank-1-0 <mayankrai368@gmail.com>
2024-10-06 19:20:17 +0530

    Create genereteParentheses.java

---
+++ genereteParentheses.java
@@ -0,0 +1,30 @@
+
+import java.util.Scanner;
+class generateParentheses {
+   
+    public static void print(int open,int close,int n,String s)
+    {
+        if(s.length()==2*n)
+        {
+            System.out.println(s);
+            return;
+        }
+        if(open<n)
+        {
+            print(open+1,close,n,s+"(");
+        }
+        if(close<open) 
+        {
+            print(open,close+1,n,s+")");
+        }
+       
+
+    }
+    public static void main(String[] args)
+    {
+        try (Scanner sc = new Scanner(System.in)) {
+            int n= sc.nextInt();
+            print(0,0,n,"");
+        }
+    }
+}
